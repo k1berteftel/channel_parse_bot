40b263fdbe06955751b37e06d81d74a8722fe62a
k1berteftel <kkulis985@gmail.com>
2025-08-25 20:09:58 +0300

    fix

--- utils/schedulers.py
+++ utils/schedulers.py
@@ -9,7 +9,7 @@ from database.action_data_class import DataInteraction
 
 async def copy_post(bot: Bot, msg_id: int, from_chat_id, chat_id: int, job_id: str, scheduler: AsyncIOScheduler):
     try:
-        await bot.copy_message(
+        await bot.forward_message(
             chat_id=chat_id,
             from_chat_id=from_chat_id,
             message_id=msg_id
